52067b74d7258d1d693574339a6604e5d1a43c52
Ariel Miki Abraham <Ariel Miki Abraham>
2017-10-09 22:31:20 +0700

    Create templates

--- socmedProject/urls.py
+++ socmedProject/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+import update_status.urls as update_status
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^status/',include(update_status,namespace='status')),
+    url(r'^$', RedirectView.as_view(url='/status/',permanent='true'), name='index'),
 ]
