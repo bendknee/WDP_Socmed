c122eb36ffda706e21f7a9bfd95ccb5728a31065
Benny William Pardede <bpardedewilliam@gmail.com>
2017-10-10 15:42:20 +0700

    Urls & Views

--- statistic/views.py
+++ statistic/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+response = {}
+
+def index(request):
+    response['friends'] = None #len(ModelsTirta.objects.all())
+    response['feeds'] = None #len(ModelsMiki.objects.all())
+    response['last_feed'] = None #ModelsMiki.
+    html = 'statistic/statistic.html'
+    render(request, html, response)
