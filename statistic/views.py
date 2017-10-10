f3412812ae99b04628d5ec770995393539c5f4a8
Benny William Pardede <bpardedewilliam@gmail.com>
2017-10-10 16:27:02 +0700

    Fix navbar bug

--- statistic/views.py
+++ statistic/views.py
@@ -4,7 +4,8 @@ response = {}
 
 def index(request):
     response['friends'] = None #len(ModelsTirta.objects.all())
-    response['feeds'] = None #len(ModelsMiki.objects.all())
-    response['last_feed'] = None #ModelsMiki.
+    feeds = None #ModelsMiki.objects.all()
+    response['feeds'] = None #len(feeds)
+    response['last_feed'] = None #ModelsMiki.objects.get(pk=len(feeds)-1)
     html = 'statistic/statistic.html'
     render(request, html, response)
