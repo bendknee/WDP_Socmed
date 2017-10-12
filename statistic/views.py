91a13419e81cb47c980baf8065dc195cd99f8d70
Benny William Pardede <bpardedewilliam@gmail.com>
2017-10-12 19:10:00 +0700

    Revamp Statistic HTML. Warning: main.css & base.html ada yang diganti!

--- statistic/views.py
+++ statistic/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from update_status.models import Status
+import profile_page.views as profile
 
 response = {}
 
 def index(request):
-    response['name'] = 'Musibah Smith'
+    response['name'] = profile.response['author']
+    response['birthday'] = profile.birthday
+    response['email'] = profile.email
     response['friends'] = '21' #len(ModelsTirta.objects.all())
-    feeds = None #ModelsMiki.objects.all()
-    response['feeds'] = '2119' #len(feeds)
-    response['last_feed'] = 'hehe AHHHHHHH' #ModelsMiki.objects.get(pk=len(feeds)-1)
+    response['feeds'] = Status.objects.count()
+    if Status.objects.count() > 0:
+        response['last_feed'] = Status.objects.all()[Status.objects.count()-1:]
+    else:
+        response['last_feed'] = "No Posts Yet!"
     html = 'statistic/statistic.html'
     return render(request, html, response)
